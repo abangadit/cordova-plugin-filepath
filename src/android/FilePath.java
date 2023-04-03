244a593ab6a937aefc2ef827e8a4c43da94d1000
abangadit <swara.ogawa@gmail.com>
2023-04-03 14:06:51 +0700

    add resolvenativepath2

--- src/android/FilePath.java
+++ src/android/FilePath.java
@@ -79,8 +79,15 @@ public class FilePath extends CordovaPlugin {
             }
 
             return true;
-        }
-        else {
+        }else if (action.equals("resolveNativePath2")) {
+            if (PermissionHelper.hasPermission(this, READ)) {
+                resolveNativePath2();
+            } else {
+                getReadPermission(READ_REQ_CODE);
+            }
+
+            return true;
+        }else {
             JSONObject resultObj = new JSONObject();
 
             resultObj.put("code", INVALID_ACTION_ERROR_CODE);
@@ -92,35 +99,89 @@ public class FilePath extends CordovaPlugin {
         return false;
     }
 
-    public void resolveNativePath() throws JSONException {
-        JSONObject resultObj = new JSONObject();
-        /* content:///... */
-        Uri pvUrl = Uri.parse(this.uriStr);
+    
+  public void resolveNativePath2() throws JSONException {
+    JSONObject resultObj = new JSONObject();
+    /* content:///... */
+    Uri pvUrl = Uri.parse(this.uriStr);
 
-        Log.d(TAG, "URI: " + this.uriStr);
+    Log.d(TAG, "URI: " + this.uriStr);
 
-        Context appContext = this.cordova.getActivity().getApplicationContext();
-        String filePath = getPath(appContext, pvUrl);
+    Context appContext = this.cordova.getActivity().getApplicationContext();
+    String filePath = getPath(appContext, pvUrl);
 
-        //check result; send error/success callback
-        if (filePath == GET_PATH_ERROR_ID) {
-            resultObj.put("code", GET_PATH_ERROR_CODE);
-            resultObj.put("message", "Unable to resolve filesystem path.");
+    final boolean isAndroidQ = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q;
 
-            this.callback.error(resultObj);
-        }
-        else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
-            resultObj.put("code", GET_CLOUD_PATH_ERROR_CODE);
-            resultObj.put("message", "Files from cloud cannot be resolved to filesystem, download is required.");
-
-            this.callback.error(resultObj);
-        }
-        else {
-            Log.d(TAG, "Filepath: " + filePath);
-
-            this.callback.success("file://" + filePath);
-        }
+    if (isAndroidQ) {
+      filePath = getDriveFilePath(pvUrl, appContext);
+    } else {
+      filePath = getPath(appContext, pvUrl);
     }
+
+    //check result; send error/success callback
+    if (filePath == GET_PATH_ERROR_ID) {
+      resultObj.put("code", GET_PATH_ERROR_CODE);
+      resultObj.put("message", "Unable to resolve filesystem path.");
+
+      this.callback.error(resultObj);
+    }
+    else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
+      resultObj.put("code", GET_CLOUD_PATH_ERROR_CODE);
+      resultObj.put("message", "Files from cloud cannot be resolved to filesystem, download is required.");
+
+      this.callback.error(resultObj);
+    }
+    else {
+      Log.d(TAG, "Filepath: " + filePath);
+
+      this.callback.success("file://" + filePath);
+    }
+  }
+  public void resolveNativePath() throws JSONException {
+    JSONObject resultObj = new JSONObject();
+    /* content:///... */
+    Uri pvUrl = Uri.parse(this.uriStr);
+
+    Log.d(TAG, "URI: " + this.uriStr);
+
+    Context appContext = this.cordova.getActivity().getApplicationContext();
+    String filePath = getPath(appContext, pvUrl);
+
+    //check result; send error/success callback
+    if (filePath == GET_PATH_ERROR_ID) {
+      resultObj.put("code", GET_PATH_ERROR_CODE);
+      resultObj.put("message", "Unable to resolve filesystem path.");
+
+      this.callback.error(resultObj);
+    } else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
+      resultObj.put("code", GET_CLOUD_PATH_ERROR_CODE);
+      resultObj.put("message", "Files from cloud cannot be resolved to filesystem, download is required.");
+
+      this.callback.error(resultObj);
+    } else {
+      Log.d(TAG, "Filepath: " + filePath);
+
+      this.callback.success("file://" + filePath);
+    }
+  }
+
+
+  public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+    for (int r : grantResults) {
+      if (r == PackageManager.PERMISSION_DENIED) {
+        JSONObject resultObj = new JSONObject();
+        resultObj.put("code", 3);
+        resultObj.put("message", "Filesystem permission was denied.");
+
+        this.callback.error(resultObj);
+        return;
+      }
+    }
+
+    if (requestCode == READ_REQ_CODE) {
+      resolveNativePath();
+    }
+  }
 
 
     public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
--- www/FilePath.js
+++ www/FilePath.js
@@ -1,14 +1,20 @@
-var exec = require('cordova/exec');
-
+var exec = require("cordova/exec");
 
 module.exports = {
-    /**
-     * Resolve native path for given content URL/path.
-     * @param {String} path  Content URL/path.
-     * @param successCallback  invoked with a native filesystem path string
-     * @param errorCallback  invoked if error occurs
-     */
-    resolveNativePath: function(path, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "FilePath", "resolveNativePath", [path]);
-    }
+  /**
+   * Resolve native path for given content URL/path.
+   * @param {String} path  Content URL/path.
+   * @param successCallback  invoked with a native filesystem path string
+   * @param errorCallback  invoked if error occurs
+   */
+  resolveNativePath: function (path, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "FilePath", "resolveNativePath", [
+      path,
+    ]);
+  },
+  resolveNativePath2: function (path, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "FilePath", "resolveNativePath2", [
+      path,
+    ]);
+  },
 };
